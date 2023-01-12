3383a80db5672107ce6532bb76ef61e5ac713f14
liuyong17 <741639435@qq.com>
2023-01-12 15:18:41 +0800

    feat: 整理文件

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,5 +1,13 @@
+// 登录之后的上下文
 export interface UserContext {
   userId: number;
   username: string;
   phoneNum: string;
+}
+
+// 校验相关
+export interface Tips {
+  key: string;
+  reg: RegExp;
+  tigs: string;
 }
--- src/utils/validate.ts
+++
@@ -1,7 +0,0 @@
-import { Tips } from '../type/conterller';
-
-export function validate(data: Record<string, any>, tips: Tips[]) {
-  return tips
-    .filter(item => item.reg.test(data[item.key]) === false)
-    .map(item => ({ key: item.key, tigs: item.tigs }));
-}
